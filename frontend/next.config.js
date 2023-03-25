4a6b393a02b447e7974f6346943e6bf1cd10d8bb
Zack <zecharyw@gmail.com>
2023-03-26 02:07:47 +0800

    Changed Strapi URL to backend to fix fetch failed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
+const dns = require('dns')
+
+dns.setDefaultResultOrder('ipv4first')
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['backend'],
   },
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
   experimental: {
     appDir: true,
+  },
+  env: {
+    NEXT_PUBLIC_STRAPI_API_URL: 'http://backend:1338/api',
+    NEXT_PUBLIC_STRAPI_URL: 'http://backend:1338',
+    NEXT_PUBLIC_SITE_URL: 'http://localhost:3000',
   },
 }
 
