4b79036e391a4c6d7f65aaf748002b2ebd5ad3b3
Zack <zecharyw@gmail.com>
2023-03-26 14:25:50 +0800

    Updated to non-docker version

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,9 +14,10 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    NEXT_PUBLIC_STRAPI_API_URL: 'http://backend:1338/api',
+    // For Docker Containers to communicate
+    // NEXT_PUBLIC_STRAPI_API_URL: 'http://backend:1338/api',
     // For local development without Docker
-    // NEXT_PUBLIC_STRAPI_API_URL: 'http://localhost:1338/api',
+    NEXT_PUBLIC_STRAPI_API_URL: 'http://localhost:1338/api',
     NEXT_PUBLIC_STRAPI_URL: 'http://localhost:1338',
     NEXT_PUBLIC_SITE_URL: 'http://localhost:3000',
   },
